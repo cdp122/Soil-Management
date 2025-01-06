5e3ae161e339c429d6f079b7d13c3372a099b5b6
Adrian Urresta <152809078+Mclxv1nx@users.noreply.github.com>
2025-01-06 09:33:35 -0500

    Login
    Cambios en login pero simples de funcionamiento visual

--- Proyecto/Frontend/src/Login.jsx
+++ Proyecto/Frontend/src/Login.jsx
@@ -1,9 +1,38 @@
+
+import './styles/Login.css'
+
 function login() {
     return (
-        <h1> Holaaa Adrian  </h1>
-
-
-
+        <div className="content">
+            <div className="form-group">
+                <label htmlFor="username"><b>Usuario</b></label>
+                <input
+                    type="text"
+                    className="username"
+                    id="username"
+                    required
+                    pattern=".{10}"
+                    title="Ingrese los 10 digitos de su cédula"
+                    autoFocus
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="password"><b>Contraseña</b></label>
+                <input
+                    type="password"
+                    className="contraseña"
+                    id="password"
+                    required
+                    pattern=".{8,}"
+                    title="Ingrese su contraseña"
+                />
+            </div>
+            <div className="form-group">
+                <button className="btn" type="submit">Iniciar Sesión</button>
+                <a href="" className="btn">Registrarse</a>
+                <a href="" className="btn recuperar">Recuperar Contraseña</a>
+            </div>
+        </div>
     )
 }
 
