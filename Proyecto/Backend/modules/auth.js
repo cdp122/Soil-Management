9ecb3d01bd436e4d7e28a4ba7bfd8634794aec7e
Carlos Puentestar <puentestarchamorrocarlosdaniel@gmail.com>
2025-01-12 20:41:56 -0500

    Prueba de funcionamiento de jsonwebtoken en Render

--- Proyecto/Backend/modules/auth.js
+++ Proyecto/Backend/modules/auth.js
@@ -1,6 +1,10 @@
+//#region [IMPORT]
 const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
 const saltRounds = 10;
+//#endregion
 
+//#region encriptaciones de contraseñas
 const encryptPassword = async (password) => {
     try {
         const hash = await bcrypt.hash(password, saltRounds);
@@ -20,5 +24,30 @@ const verifyPassword = async (password, hash) => {
         throw error;
     }
 };
+//#endregion
+
+//#region Generación de tokens
+function generateAccessToken(user) {
+    return jwt.sign(user, "telepass", { expiresIn: "60m" });
+}
+
+function validateToken(req, res, next) {
+    try {
+        const token = req.headers['authorization'];
+        if (!token) res.redirect('error/404');
+
+        jwt.verify(token, "telepass", (err, user) => {
+            if (err) {
+                console.log("Autenticación por usuario fallida.");
+                res.json({ message: "AUTENTICACIÓN FALLIDA" });
+            } else {
+                console.log("Autenticación exitosa.");
+                req.user = user;
+                next();
+            }
+        })
+    }
+    catch { res.redirect('error/404'); }
+}
 
 module.exports = { encryptPassword, verifyPassword };
