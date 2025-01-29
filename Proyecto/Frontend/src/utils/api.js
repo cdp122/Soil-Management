4ef914701ef9fc31af9c6803ca5f2a225c3c9ffb
Alex_c2004 <135248384+Aleci3@users.noreply.github.com>
2025-01-28 20:44:45 -0500

    Update api.js

--- Proyecto/Frontend/src/utils/api.js
+++ Proyecto/Frontend/src/utils/api.js
@@ -4,7 +4,6 @@ class Api {
     }
 
     async nuevaParcela(data) {
-        await console.log(data);
         try {
             const response = await fetch(`${this.api_url}/nuevaparcela`, {
                 method: "POST",
