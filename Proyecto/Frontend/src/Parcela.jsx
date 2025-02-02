976bfc7f19023159c6f5ef4b422ecb505332cead
Adrian Urresta <152809078+Mclxv1nx@users.noreply.github.com>
2025-02-01 19:38:01 -0500

    Quitar ngrok
    asd

--- Proyecto/Frontend/src/Parcela.jsx
+++ Proyecto/Frontend/src/Parcela.jsx
@@ -273,8 +273,7 @@ function Parcela({ parcelID, parcelName, parcelType, isOpen, isParcelaSelecciona
                 method: "DELETE",
                 headers: {
                     Authorization: token,
-                    "Content-Type": "application/json",
-                    'ngrok-skip-browser-warning': true
+                    "Content-Type": "application/json"
                 },
             });
 
